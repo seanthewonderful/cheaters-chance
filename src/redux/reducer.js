5d64cd3e0099261d330c96eeaa351379c36b866f
Sean Fagan <faganseanp@gmail.com>
2024-03-13 15:11:57 -0600

    redux and router basic setup

---
+++ src/redux/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+  user: {}
+}
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+       ...state,
+        user: action.payload
+      }
+    default:
+      return state
+  }
+}
