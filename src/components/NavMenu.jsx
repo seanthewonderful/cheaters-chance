ad085e0338e672051120c5c4b1280ecdd378ac26
Sean Fagan <faganseanp@gmail.com>
2024-03-15 23:56:44 -0600

    navmenu hamburger icon dropdown basic functionality.

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import hamburgerMenu from '/icons/hamburgerMenu.svg'
 
 const NavMenu = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
-    <div>NavMenu</div>
-  )
+    <div>
+      <button
+        className='hamburger-button' 
+        onClick={toggleMenu}
+        >
+          <img src={hamburgerMenu} alt="" />
+      </button>
+      {isOpen && (
+        <div className='menu-items'>
+          {/* Menu Items */}
+            <NavLink to="#">Profile</NavLink>
+          
+            <NavLink to="#">Join</NavLink>
+         
+            <NavLink to="#">Host</NavLink>
+      
+            <NavLink to="#">Rules</NavLink>     
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default NavMenu
