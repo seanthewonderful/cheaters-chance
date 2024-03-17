c95277a40d717ff77c157767b1bfa450d99a99c9
Sean Fagan <faganseanp@gmail.com>
2024-03-16 20:09:31 -0600

    added error page and connected to routes

---
+++ src/components/pages/errors/NotFound.jsx
@@ -0,0 +1,22 @@
+
+const NotFound = () => {
+  return (
+    <div id='not-found-div'>
+
+      <div>
+        <h1>404 Error: Page not found</h1>
+      </div>
+
+      <div>
+        <img
+          id="not-found-img"
+          src="https://gifdb.com/images/high/wicked-villan-darth-vader-noooo-shouting-x8zaqkytb2n4q5m0.gif"
+          alt=""
+        />
+      </div>
+      
+    </div>
+  )
+}
+
+export default NotFound
