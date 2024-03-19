bb31fa1da05dc90471c313ba60e2d5a182e7d0c7
kylebaugh <kylebaugh1@gmail.com>
2024-03-19 12:13:45 -0600

    starting sockets

--- backend/server/server.js
+++ backend/server/server.js
@@ -3,16 +3,35 @@ import session from 'express-session'
 import ViteExpress from 'vite-express'
 import userFunctions from './controllers/userController.js'
 import gameFunctions from './controllers/gameController.js'
+import http from 'http'
+import {Server} from 'socket.io'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
-app.use(session({
+
+app.use(cors({
+    origin: 'http://localhost:4000',
+    credentials: true
+}))
+
+const sessionMiddleware = session({
     secret: 'theOneRing',
     saveUninitialized: false,
     resave: false
-}))
+})
+
+app.use(sessionMiddleware)
+
+app.use(express.static('src'))
+
+const httpServer = http.createServer(app)
+const io = new Server(httpServer)
+io.engine.use(sessionMiddleware)
+
+// io.on('connection', (socket))
 
 // ACCOUNT ENDPOINTS
 const {register, login, logout, sessionCheck} = userFunctions
@@ -29,4 +48,8 @@ app.get('/api/allGames', allGames)
 app.post('/api/joinGame', joinGame)
 app.get('/api/startGame', startGame)
 
-ViteExpress.listen(app, 2319, () => console.log('Keep it secret, keep it safe on http://localhost:2319'))
+// ViteExpress.listen(app, 2319, () => console.log('Keep it secret, keep it safe on http://localhost:2319'))
+
+httpServer.listen(2319, () => console.log('Keep it secret, keep it safe on http://localhost:2319'))
+
+ViteExpress.bind(app, httpServer)
